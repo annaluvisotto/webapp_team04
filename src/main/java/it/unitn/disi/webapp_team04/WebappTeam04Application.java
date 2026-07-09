ea103b55e21bdee215e0c603ff61aa864f29fb10
Anna <anna.luvisotto@gmail.com>
2026-07-09 19:38:05 +0200

    First commit

---
+++ src/main/java/it/unitn/disi/webapp_team04/WebappTeam04Application.java
@@ -0,0 +1,13 @@
+package it.unitn.disi.webapp_team04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappTeam04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappTeam04Application.class, args);
+    }
+
+}
---
+++ src/test/java/it/unitn/disi/webapp_team04/WebappTeam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unitn.disi.webapp_team04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappTeam04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
