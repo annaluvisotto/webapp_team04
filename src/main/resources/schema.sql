1c540145fb2f6f4b8588eb92c0f16fade2e8d3b9
Anna <anna.luvisotto@gmail.com>
2026-08-16 15:31:40 +0200

    revisione db e organizzazione repositories

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,39 +1,40 @@
 CREATE TABLE IF NOT EXISTS Users(
-    ID INT PRIMARY KEY,
-    username varchar(32) NOT NULL,
-    password varchar(32) NOT NULL,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    username varchar(32) NOT NULL UNIQUE,
+    password varchar(512) NOT NULL,
     enabled boolean NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS Authorities(
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     username varchar(32) NOT NULL,
-    ruolo enum('ROLE_ADMIN', 'ROLE_USER_PROVA', 'ROLE_USER_BASIC', 'ROLE_USER_PRO') NOT NULL,
-    FOREIGN KEY (ID) REFERENCES Users(ID) ON DELETE CASCADE
+    authority enum('ROLE_ADMIN', 'ROLE_USER_PROVA', 'ROLE_USER_BASIC', 'ROLE_USER_PRO') NOT NULL,
+    FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS Users_info(
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     nome varchar(32) NOT NULL,
-    nome varchar(32) NOT NULL,
+    cognome varchar(32) NOT NULL,
     data_nascita date,
     email varchar(32) NOT NULL,
     username varchar(32) NOT NULL,
     password varchar(32) NOT NULL,
-    data_reg date,
+    data_reg date NOT NULL,
     FOREIGN KEY (ID) REFERENCES Users(ID) ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS Default_Trainings(
     ID_Training INT,
-    ID_User varchar(32) NOT NULL,
+    ID_User INT NOT NULL,
     esecuzioni INT,
-    PRIMARY KEY (ID_Training, ID_User)
+    PRIMARY KEY (ID_Training, ID_User),
+    FOREIGN KEY (ID_User) REFERENCES Users(ID)
     );
 
 CREATE TABLE IF NOT EXISTS Personalized_Trainings(
     ID_Training INT,
-    ID_User varchar(32) NOT NULL,
+    ID_User INT NOT NULL,
     nome_allenamento varchar(32) NOT NULL,
     esecuzioni INT,
     PRIMARY KEY (ID_Training, ID_User),
@@ -42,7 +43,7 @@ CREATE TABLE IF NOT EXISTS Personalized_Trainings(
 
 CREATE TABLE IF NOT EXISTS Personalized_Trainings_info(
     ID_Training INT,
-    ID_User varchar(32) NOT NULL,
+    ID_User INT NOT NULL,
     nome_esercizio varchar(32) NOT NULL,
     numero_serie INT,
     numero_ripetizioni INT,
@@ -50,11 +51,11 @@ CREATE TABLE IF NOT EXISTS Personalized_Trainings_info(
     FOREIGN KEY (ID_User) REFERENCES Users(ID) ON DELETE CASCADE
     );
 
-CREATE TABLE IF NOT EXISTS Recensioni(
-    ID_Recensione INT PRIMARY KEY,
-    ID_User varchar(32) NOT NULL,
-    titolo varchar(32) NOT NULL,
-    testo varchar(512) NOT NULL,
+CREATE TABLE IF NOT EXISTS Reviews(
+    ID_Review INT AUTO_INCREMENT PRIMARY KEY,
+    ID_User INT NOT NULL,
+    title varchar(32) NOT NULL,
+    text varchar(512) NOT NULL,
     FOREIGN KEY (ID_User) REFERENCES Users(ID)
     );
 
