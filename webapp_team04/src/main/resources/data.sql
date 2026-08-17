39cd4c71c5040fd711d56d3fba50b7452ce47b76
Anna <anna.luvisotto@gmail.com>
2026-08-17 16:49:20 +0200

    modifica data.sql

--- webapp_team04/src/main/resources/data.sql
+++ webapp_team04/src/main/resources/data.sql
@@ -8,3 +8,19 @@ INSERT INTO Authorities VALUES (1, admin, ROLE_ADMIN);
 INSERT INTO Authorities VALUES (2, sofia, ROLE_USER_PRO);
 INSERT INTO Authorities VALUES (3, anna, ROLE_USER_BASIC);
 INSERT INTO Authorities VALUES (4, giovanna, giovanna, ROLE_USER_PROVA);
+
+INSERT INTO Users (username, password, enabled)
+VALUES ('admin#team_04', 'adm_id_04', true);
+
+INSERT INTO Authorities (username, authority)
+VALUES ('admin#team_04', 'ROLE_ADMIN');
+
+INSERT INTO Users_info (ID, nome, cognome, data_nascita, email, data_reg)
+VALUES (
+           (SELECT ID FROM Users WHERE username = 'admin#team_04'),
+           'Anna',
+           'Luvisotto',
+           '2005-03-21',
+           'anna@email.com',
+           CURRENT_DATE
+       );
