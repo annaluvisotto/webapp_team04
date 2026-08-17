ac5793a86682442227e278e8775c939fab90edbe
Sofia Cestari <sofia.cestari@studenti.unitn.it>
2026-08-17 12:38:47 +0200

    file di configurazione aggiunti

---
+++ REST_service_team04/src/test/java/it/unitn/disi/Rest_service_team04/RestServiceTeam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unitn.disi.rest_service_team04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceTeam04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
