4c4bfe75d23e6c15c13ab4b2e1f4c4bffac64699
Sofia Cestari <sofia.cestari@studenti.unitn.it>
2026-08-18 14:35:32 +0200

    correzione errori sql1

--- webapp_team04/src/main/resources/data.sql
+++ webapp_team04/src/main/resources/data.sql
@@ -1,14 +1,3 @@
-SELECT 1; -- non significa nulla, l'ho aggiunto perchè data.sql non può essere vuoto
-INSERT INTO Users VALUES (1, admin, password, TRUE);
-INSERT INTO Users VALUES (2, sofia, sofia, TRUE);
-INSERT INTO Users VALUES (3, anna, anna, TRUE);
-INSERT INTO Users VALUES (4, giovanna, giovanna, TRUE);
-
-INSERT INTO Authorities VALUES (1, admin, ROLE_ADMIN);
-INSERT INTO Authorities VALUES (2, sofia, ROLE_USER_PRO);
-INSERT INTO Authorities VALUES (3, anna, ROLE_USER_BASIC);
-INSERT INTO Authorities VALUES (4, giovanna, giovanna, ROLE_USER_PROVA);
-
 INSERT INTO Users (username, password, enabled)
 VALUES ('admin#team_04', 'adm_id_04', true);
 
