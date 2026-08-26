e749a1a5a7de292b3a4324f8ec3f2bf1c1c20c2e
Anna <anna.luvisotto@gmail.com>
2026-08-26 11:14:56 +0200

    js registrazione

--- webapp_team04/src/main/java/it/unitn/disi/webapp_team04/controllers/MainController.java
+++ webapp_team04/src/main/java/it/unitn/disi/webapp_team04/controllers/MainController.java
@@ -36,13 +36,13 @@ public class MainController {
     //crea un oggetto User riempiendo i campi con i valori inseriti dall'utente
     public String adduser(@RequestParam String nome,
                           @RequestParam String cognome,
-                          @RequestParam String data_nascita,
+                          @RequestParam String data,
                           @RequestParam String email,
                           @RequestParam String username,
                           @RequestParam String password,
                           @RequestParam String ruolo,
                           Model model){
-        if(!checkUser.addUser(nome, cognome, data_nascita, email, username, password, ruolo)){
+        if(!checkUser.addUser(nome, cognome, data, email, username, password, ruolo)){
             model.addAttribute("errore", "#team_04: This username is already taken, provide another one");
             return "public/signup";
         }
---
+++ webapp_team04/src/main/resources/static/script.js
@@ -0,0 +1,93 @@
+const formSignup = document.getElementById("signup");
+const dataNascita = document.getElementById("data");
+const erroreData = document.getElementById("errore-data");
+const password = document.getElementById("password");
+const errorePassword = document.getElementById("errore-formato");
+const confermaPassword = document.getElementById("passwordRipetuta");
+const erroreConferma = document.getElementById("errore-conferma");
+const btnReset = document.getElementById("res");
+
+//gestione "submit" registrazione
+formSignup.addEventListener("submit", function (e){
+    var formValido = true;
+
+    //controllo formato data GG/MM/AAAA
+    const dataSpezzata = dataNascita.value.split('/');
+    if(dataSpezzata.length !== 3 || dataSpezzata[0].length !== 2 || dataSpezzata[1].length !== 2 || dataSpezzata[2].length !== 4){
+        erroreData.innerHTML = "Formato della data non valido (GG/MM/AAAA)";
+        formValido = false;
+    }
+    //controllo utente maggiorenne (solo se il formato della data è corretto)
+    else{
+        const giorno = parseInt(dataSpezzata[0], 10);
+        const mese = parseInt(dataSpezzata[1], 10);
+        const anno = parseInt(dataSpezzata[2], 10);
+        const nascita = new Date(anno, mese-1, giorno);
+        const oggi = new Date();
+        if(!isMaggiorenne(nascita, oggi)){
+            erroreData.innerHTML = "Devi essere maggiorenne per poterti registrare";
+            formValido = false;
+        }
+        else{
+            erroreData.innerHTML = "";
+        }
+    }
+
+    //controllo formato password
+    const pattern = /id_04/;
+    if(!pattern.test(password.value) || password.value.length !== 8){
+        errorePassword.innerHTML = "La password deve contenere 'id_04' e dev'essere lunga 8 caratteri";
+        formValido = false;
+    }
+    else{
+        errorePassword.innerHTML = "";
+    }
+
+    //controllo corrispondenza tra le 2 password
+    if(password.value !== confermaPassword.value){
+        erroreConferma.innerHTML = "Le due password non coincidono";
+        formValido = false;
+    }
+    else{
+        erroreConferma.innerHTML = "";
+    }
+
+    if(formValido === false){
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+});
+
+function isMaggiorenne(nascita, oggi){
+    if(nascita.getFullYear() > (oggi.getFullYear() - 18)){
+        return false;
+    }
+    else{
+        if(nascita.getFullYear() === (oggi.getFullYear() - 18)){
+            if(nascita.getMonth() > oggi.getMonth()){
+                return false;
+            }
+            else{
+                if(nascita.getMonth() === oggi.getMonth()){
+                    return nascita.getDate() <= oggi.getDate();
+                }
+                else{
+                    return true;
+                }
+            }
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+//gestione "reset" registrazione
+btnReset.addEventListener("click", function () {
+    erroreData.innerHTML = "";
+    errorePassword.innerHTML = "";
+    erroreConferma.innerHTML = "";
+});
+
+
